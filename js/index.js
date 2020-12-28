c8b01a3c700b167c1d4d024686b75dd28652ceea
Luiz Gustavo <luiz@pop-os.localdomain>
2020-12-28 12:18:38 -0300

    Removing redundancies in the index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-function change_content() {
-  setTimeout(() => {
+function change_content(change) {
+  if (change === true) {
+    setTimeout(() => {
+      const button_content = document.getElementById("create-note-button");
+      button_content.innerHTML = `<i class="fa fa-plus" aria-hidden="true" style="margin-right: 5px;"></i> Criar uma nota`;
+    }, 600);
+  } else {
     const button_content = document.getElementById("create-note-button");
-    button_content.innerHTML = `<i class="fa fa-plus" aria-hidden="true" style="margin-right: 5px;"></i> Criar uma nota`;
-  }, 600);
-}
-
-function change_content_back() {
-  const button_content = document.getElementById("create-note-button");
-  button_content.innerHTML = `<div id="create-note-button-content">
+    button_content.innerHTML = `<div id="create-note-button-content">
   <i class="fa fa-plus" aria-hidden="true"></i>
 </div>`;
+  }
 }
