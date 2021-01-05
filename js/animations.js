adaa72747e900e223dea684a6ba0b13250f5a90b
Luiz Gustavo <gugamatias7@gmail.com>
2021-01-05 12:25:41 -0300

    Adding all animations to a unique file

--- js/animations.js
+++ js/animations.js
@@ -7,8 +7,8 @@ function change_edit_button_content(change) {
   } else {
     const button_content = document.getElementById("edit-button");
     button_content.innerHTML = `<div id="edit-note-button-content">
-      <i class="fa fa-pencil" aria-hidden="true"></i>
-  </div>`;
+        <i class="fa fa-pencil" aria-hidden="true"></i>
+    </div>`;
   }
 }
 
@@ -21,8 +21,8 @@ function change_delete_button_content(change) {
   } else {
     const button_content = document.getElementById("delete-button");
     button_content.innerHTML = `<div id="edit-note-button-content">
-      <i class="fa fa-trash-o" aria-hidden="true"></i>
-  </div>`;
+        <i class="fa fa-trash-o" aria-hidden="true"></i>
+    </div>`;
   }
 }
 
@@ -35,7 +35,21 @@ function change_create_button_content(change) {
   } else {
     const button_content = document.getElementById("create-button");
     button_content.innerHTML = `<div id="edit-note-button-content">
-      <i class="fa fa-check" aria-hidden="true"></i>
+        <i class="fa fa-check" aria-hidden="true"></i>
+    </div>`;
+  }
+}
+
+function change_content(change) {
+  if (change === true) {
+    setTimeout(() => {
+      const button_content = document.getElementById("create-note-button");
+      button_content.innerHTML = `<i class="fa fa-plus" aria-hidden="true" style="margin-right: 5px;"></i> Criar uma nota`;
+    }, 500);
+  } else {
+    const button_content = document.getElementById("create-note-button");
+    button_content.innerHTML = `<div id="create-note-button-content">
+    <i class="fa fa-plus" aria-hidden="true"></i>
   </div>`;
   }
 }
